d014117646cc5de001554c49131cb123d301d19d
Flavia <flavia.olsantos2@gmail.com>
2026-03-17 20:31:19 -0300

    fix: seed admin user on startup

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +19,15 @@ class DatabaseSeeder extends Seeder
                 'updated_at' => now(),
             ]);
         }
+
+        DB::table('users')->insertOrIgnore([
+            'nome'       => 'Administrador',
+            'email'      => 'admin@assembleia.com',
+            'password'   => Hash::make('Admin@2026'),
+            'perfil'     => 'admin',
+            'cidade_id'  => null,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
     }
 }
